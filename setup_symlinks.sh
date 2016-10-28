d93ab22de0a7a778e50a0199b77be484cc40cd56
Zachary Alden Meadows <zmeadows@lxplus020.cern.ch>
2016-10-28 18:41:12 +0200

    asdf

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-echo $HOSTNAME
+echo "SETTING UP DOTFILES ON: $HOSTNAME\n"
 
-
-
-if [[ $HOSTNAME == *"macbook"* ]]
-then
-    echo "on macbook";
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    echo "INSTALLING OH-MY-ZSH"
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
-if [[ $HOSTNAME == *"lxplus"* ]]
-then
-    echo "on lxplus";
+dir=~/dotfiles                           # dotfiles directory
+olddir=~/dotfiles_old                    # old dotfiles backup directory
+files="vimrc" # list of files/folders to symlink in homedir
+
+if [[ $HOSTNAME == *"macbook"* ]]; then
+elif [[ $HOSTNAME == *"lxplus"* ]]; then
+elif [[ $HOSTNAME == *"titan"* ]]; then
+else
+    echo "UNRECOGNIZED HOSTNAME: $HOSTNAME"
+    echo "EXITING..."
+    exit 1
 fi
+
