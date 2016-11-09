85e2e5ee7f9183a42901bf3b94aca19ece47f0a6
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-11-09 10:00:37 -0500

    properly checking remote origin for changes

--- monitor_updates.sh
+++ monitor_updates.sh
@@ -8,7 +8,7 @@ fi
 cd $dir
 git status > /dev/null 2>&1
 
-git diff-index --quiet HEAD --
+git diff --quiet origin/master..HEAD
 if [ $? -ne 0 ]; then
     echo "NOT in sync with github dotfiles!"
 else
